714931fba5f2ce3bee7e4a25b31d9f70a898a223
Vuk Dukic <vuk.dukic@clara.com>
2018-03-09 09:41:41 -0800

    Update Helper.js

--- Apps/expenseTracker.app/Helper.js
+++ Apps/expenseTracker.app/Helper.js
@@ -1,3 +1,9 @@
+/*
+First we get the array of expenses from the expenses attribute. 
+Then we add the new expense “record” to it. 
+Then we update (set) the expenses attribute with the changed array.
+*/
+
 ({
     createExpense: function(component, expense) {
         var theExpenses = component.get("v.expenses");
