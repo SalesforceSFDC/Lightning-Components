cc17a3cf541438718791c96f1d6972a92dec2f73
Vuk Dukic <vuk.dukic@clara.com>
2018-03-09 09:21:04 -0800

    Create Controller.js

---
+++ Apps/expenseTracker.app/Controller.js
@@ -0,0 +1,16 @@
+({
+    clickCreate: function(component, event, helper) {
+        var validExpense = component.find('expenseform').reduce(function (validSoFar, inputCmp) {
+            // Displays error messages for invalid fields
+            inputCmp.showHelpMessageIfInvalid();
+            return validSoFar && inputCmp.get('v.validity').valid;
+        }, true);
+        // If we pass error checking, do some real work
+        if(validExpense){
+            // Create the new expense
+            var newExpense = component.get("v.newExpense");
+            console.log("Create expense: " + JSON.stringify(newExpense));
+            helper.createExpense(component, newExpense);
+        }
+    }
+})
